34e82c8624ef892df6b69ba4fb4da04f6b5f2a75
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-08 18:20:30 +0300

    Create app done | Router intro done

---
+++ react-router-example/src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return <h1>Home Page</h1>;
+};
+
+const About = () => {
+  return <h1>About Page</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+);
