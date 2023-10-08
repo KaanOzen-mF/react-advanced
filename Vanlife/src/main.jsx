39d94bb231633791615da7525ecaa50e6ec1a2d2
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-08 20:12:32 +0300

    Main.jsx updated for routes

--- Vanlife/src/main.jsx
+++ Vanlife/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <footer>
+        <p>Ⓒ 2022 #VANLIFE</p>
+      </footer>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
