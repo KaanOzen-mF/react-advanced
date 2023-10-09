4ad6f08dd830c1778554a5843c75bb3609676485
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-09 14:53:29 +0300

    Vans.jsx routes done

--- Vanlife/src/main.jsx
+++ Vanlife/src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./server";
 
 const App = () => {
   return (
@@ -13,12 +15,14 @@ const App = () => {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
 
       <footer>
