a3c8790bbd3536b3bdd7b5876cbd4c14d3a7ee16
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-09 10:12:38 +0300

    Home.jsx design element added

--- Vanlife/src/pages/Home.jsx
+++ Vanlife/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home-page-container">
       <h1>You got the travel plans, we got the travel vans.</h1>
       <h3>
         Add adventure to your life by joining the #vanlife movement. Rent the
