6c9972abff1f4fb89605259daca96f96cceb4939
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-10 10:03:43 +0300

    Vans.jsx done

--- Vanlife/src/pages/Vans.jsx
+++ Vanlife/src/pages/Vans.jsx
@@ -1,36 +1,44 @@
 import React from "react";
 
-/**
- * Challenge: Fetch and map over the data to display it on
- * the vans page. For an extra challenge, spend time styling
- * it to look like the Figma design.
- *
- * Hints:
- * 2. What React hook would you use to fetch data as soon as the
- *    Vans page loads, and only fetch it the one time?
- */
-
 export default function Vans() {
-  const [vansData, setVansData] = React.useState({});
+  const [vansData, setVansData] = React.useState([]);
 
   React.useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
-      .then((data) => setVansData(data));
+      .then((data) => setVansData(data.vans));
   }, []);
 
-  console.log(vansData);
+  const vanElements = vansData.map((van) => (
+    <div key={van.id} className="card">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <div className="price-container">
+          <p>${van.price}</p>
+          <span className="day">/day</span>
+        </div>
+      </div>
+      <i className={`van-type ${van.type} selected`}>
+        {van.type.charAt(0).toUpperCase() + van.type.slice(1)}
+      </i>
+    </div>
+  ));
+
   return (
     <div className="vans-container">
       <h1>Explore our van options</h1>
-      <div className="filter-button">
-        <ul>
+
+      <div className="filter-container">
+        <ul className="filter-buttons">
           <li>Simple</li>
           <li>Luxury</li>
           <li>Rugged</li>
         </ul>
         <p>Clear Filters</p>
       </div>
+
+      <div className="card-container">{vanElements}</div>
     </div>
   );
 }
