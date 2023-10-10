f0f50f43496d6536a312461ea55c33c7c65343f7
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-10 16:54:57 +0300

    VansDetail page nearly done

--- Vanlife/src/pages/VansDetail.jsx
+++ Vanlife/src/pages/VansDetail.jsx
@@ -1,12 +1,46 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { MdKeyboardBackspace } from "react-icons/md";
 
 export default function VansDetail() {
   const params = useParams();
-  console.log(params);
+  const [van, setVan] = React.useState([null]);
+
+  React.useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
+
+  console.log(van);
+
   return (
     <div>
-      <p>Vans Detail</p>
+      {van ? (
+        <div className="van-detail-container">
+          <Link to="/vans" className="back-button">
+            <MdKeyboardBackspace size={24} />
+            <p>Back to all vans</p>
+          </Link>
+          <img className="van-image" src={van.imageUrl} />
+          <i id="van-type" className={`van-type ${van.type} selected`}>
+            {van.type
+              ? van.type.charAt(0).toUpperCase() + van.type.slice(1)
+              : ""}
+          </i>
+          <p className="van-title">{van.name}</p>
+
+          <div className="van-detail-price">
+            <p>${van.price}</p>
+            <span>/day</span>
+          </div>
+
+          <p className="van-description">{van.description}</p>
+          <button className="van-button">Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </div>
   );
 }
