16ae893946eca25b07809bc3241a9268d2fe71fd
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-10 14:12:49 +0300

    Link to VansDetail.jsx started

--- Vanlife/src/pages/Vans.jsx
+++ Vanlife/src/pages/Vans.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Vans() {
   const [vansData, setVansData] = React.useState([]);
@@ -9,19 +10,26 @@ export default function Vans() {
       .then((data) => setVansData(data.vans));
   }, []);
 
+  /**
+   * Challenge: Wrap the contents of the "van-tile" div in a
+   * Link that sends the user to `/vans/${van-id-here}`.
+   */
+
   const vanElements = vansData.map((van) => (
     <div key={van.id} className="card">
-      <img src={van.imageUrl} />
-      <div className="van-info">
-        <h3>{van.name}</h3>
-        <div className="price-container">
-          <p>${van.price}</p>
-          <span className="day">/day</span>
+      <Link to={`/vans/${van.id}`} className="card">
+        <img src={van.imageUrl} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <div className="price-container">
+            <p>${van.price}</p>
+            <span className="day">/day</span>
+          </div>
         </div>
-      </div>
-      <i className={`van-type ${van.type} selected`}>
-        {van.type.charAt(0).toUpperCase() + van.type.slice(1)}
-      </i>
+        <i className={`van-type ${van.type} selected`}>
+          {van.type.charAt(0).toUpperCase() + van.type.slice(1)}
+        </i>
+      </Link>
     </div>
   ));
 
