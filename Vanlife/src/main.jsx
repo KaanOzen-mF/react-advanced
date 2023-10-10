fd0e5640956da61f0fa6c9fa776e21ed9b496d37
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-10 14:17:23 +0300

    Vansdetail route added

--- Vanlife/src/main.jsx
+++ Vanlife/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import VansDetail from "./pages/VansDetail";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./server";
 
@@ -23,6 +24,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VansDetail />} />
       </Routes>
 
       <footer>
