4825a592d4accceec4b24ede2c4ead3a4e423bd5
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-15 20:26:32 +0300

    Navigation link focus current page added

--- Vanlife/src/components/Header.jsx
+++ Vanlife/src/components/Header.jsx
@@ -1,18 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 //* Link elements for helping render and routing another pages
 
 export default function Header() {
+  const style = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
   return (
     <header>
       <Link className="site-logo" to="/">
         #VanLife
       </Link>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
+        <NavLink style={({ isActive }) => (isActive ? style : null)} to="/host">
+          Host
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? style : null)}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink style={({ isActive }) => (isActive ? style : null)} to="/vans">
+          Vans
+        </NavLink>
       </nav>
     </header>
   );
