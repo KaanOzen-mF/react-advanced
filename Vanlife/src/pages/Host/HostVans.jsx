870b970d7bcdc49160c01ea0e41ab0f5c1d0aabd
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-17 08:50:18 +0300

    Host vans page added

---
+++ Vanlife/src/pages/Host/HostVans.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Vans() {
+  //*State for keep vans data
+  const [vansData, setVansData] = React.useState([]);
+
+  //*Use effect for fetch api datas, dependency array empty(fetch when render page every time)
+  React.useEffect(() => {
+    fetch("/api/host/vans")
+      .then((res) => res.json())
+      .then((data) => setVansData(data.vans));
+  }, []);
+
+  const vansElement = vansData.map((van) => (
+    <Link to={`/host/vans/${van.id}`}>
+      <div className="host-vans-card" id={van.id}>
+        <img src={van.imageUrl} alt="" />
+        <div>
+          <p className="host-vans-name">{van.name}</p>
+          <p className="host-vans-detail">{van.description}</p>
+          <p className="host-vans-price">${van.price}/day</p>
+        </div>
+      </div>
+    </Link>
+  ));
+  return (
+    <div className="host-vans-page">
+      <h1>Your Listed Vans</h1>
+      {vansData.length > 0 ? (
+        <div className="host-vans-page ">{vansElement}</div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
+}
