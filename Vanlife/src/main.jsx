727b618046f360ebca90e271bfcacf1c71913d05
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-17 08:51:49 +0300

    Host Page routers added

--- Vanlife/src/main.jsx
+++ Vanlife/src/main.jsx
@@ -11,7 +11,21 @@ import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
 import HostLayout from "./components/HostLayout";
-
+import HostVans from "./pages/Host/HostVans";
+import HostVansDetail from "./pages/Host/HostVansDetail";
+/**
+ * Challenge: add the /host/vans and /host/vans/:id routes, as well
+ * as the "Vans" link in the Host navbar.
+ *
+ * For now, just create the stubbed-out version of the pages (i.e.
+ * components that just render an <h1>). Don't worry about adding
+ * navigation from /host/vans to /host/vans/:id yet - the link to
+ * /host/vans is enough for now.
+ *
+ * When deciding whether or not to use nested routes, keep in mind
+ * what will/won't be shared between these two pages. See the Figma
+ * design file (or the screenshots) to help guide your choice.
+ */
 const App = () => {
   return (
     <BrowserRouter>
@@ -28,6 +42,8 @@ const App = () => {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVansDetail />} />
           </Route>
         </Route>
       </Routes>
