07d5edf74b5c53a67742b78e19b308d323947c53
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-24 16:20:29 +0300

    Back button fixed according to link router

--- Vanlife/src/pages/Vans/VansDetail.jsx
+++ Vanlife/src/pages/Vans/VansDetail.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useLocation } from "react-router-dom";
 import { MdKeyboardBackspace } from "react-icons/md";
 
 export default function VansDetail() {
+  const location = useLocation();
   //*Params use for when render vans detail page take id
   const params = useParams();
   //*State for vans data, initial value is null because maybe when routes it can not match id
@@ -16,15 +17,20 @@ export default function VansDetail() {
       .then((data) => setVan(data.vans));
   }, [params.id]);
 
+  const search = location.state?.search || "";
   return (
     //* My initial van is null and this situation maybe creates some bug. This
     //*conditional render for to prevent this situation
     <div>
       {van ? (
         <div className="van-detail-container">
-          <Link to="/vans" className="back-button">
+          <Link to={`/vans/${search}`} className="back-button">
             <MdKeyboardBackspace size={24} />
-            <p>Back to all vans</p>
+            {location.state.type ? (
+              <p>Back {location.state.type} to vans</p>
+            ) : (
+              <p>Back to all vans</p>
+            )}
           </Link>
           <img className="van-image" src={van.imageUrl} />
           {/*According to vant type class different color shown users*/}
