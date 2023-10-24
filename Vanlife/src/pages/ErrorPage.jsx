9da50d61eda65aa65167b4f76e91b89f7ba24a08
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-24 17:28:21 +0300

    404 page added

---
+++ Vanlife/src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function errorPage() {
+  return (
+    <div className="error-page-container">
+      <h1>Sorry, the page you were looking for was not found.</h1>
+      <button>Return to home</button>
+    </div>
+  );
+}
