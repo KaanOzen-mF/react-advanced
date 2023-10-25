776b729ca5d412db211f3ec464a7c351df1f2781
KaanOzen-mF <kaanozn98@gmail.com>
2023-10-25 11:27:28 +0300

    main.jsx routers added

--- Vanlife/src/main.jsx
+++ Vanlife/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./server";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -16,9 +16,11 @@ import HostVansPricing from "./pages/Host/HostVansPricing";
 import HostVansInfo from "./pages/Host/HostVansInfo";
 import HostVansPhotos from "./pages/Host/HostVansPhotos";
 import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/Login";
 
 import Layout from "./components/Layout";
 import HostLayout from "./components/HostLayout";
+import AuthLayout from "./components/AuthLayout";
 
 const App = () => {
   return (
@@ -29,21 +31,23 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
-
           <Route path="vans/:id" element={<VansDetail />} />
+          <Route path="login" element={<Login />} />
 
-          <Route path="host" element={<HostLayout />}>
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-
-            <Route path="vans/:id" element={<HostVansDetail />}>
-              <Route index element={<HostVansInfo />} />
-              <Route path="pricing" element={<HostVansPricing />} />
-              <Route path="photos" element={<HostVansPhotos />} />
+          <Route element={<AuthLayout />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVansDetail />}>
+                <Route index element={<HostVansInfo />} />
+                <Route path="pricing" element={<HostVansPricing />} />
+                <Route path="photos" element={<HostVansPhotos />} />
+              </Route>
             </Route>
           </Route>
+
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
