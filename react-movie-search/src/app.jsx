24db6a482a0c56c20034fd6b035ab4d171832fea
KaanOzen-mF <kaanozn98@gmail.com>
2023-11-15 10:42:32 +0300

    app.jsx added

--- react-movie-search/src/app.jsx
+++ react-movie-search/src/app.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import SearchMovies from "./components/SearchMovies";
 
 export default function App() {
   return (
     <div className="container">
       <h1 className="title">Test</h1>
+      <SearchMovies />
     </div>
   );
 }
