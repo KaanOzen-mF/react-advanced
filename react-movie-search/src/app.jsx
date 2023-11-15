8f827acfa1db38b99a625d73c6f66ab763b40803
KaanOzen-mF <kaanozn98@gmail.com>
2023-11-15 15:05:49 +0300

    Serie compontent added

--- react-movie-search/src/app.jsx
+++ react-movie-search/src/app.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import SearchMovies from "./components/SearchMovies";
+import SearchMovies from "./components/movieComponents/SearchMovies";
+import SearchSeries from "./components/serieComponents/SearchSeries";
 
 export default function App() {
   return (
     <div className="container">
       <h1 className="title">Movie Search</h1>
       <SearchMovies />
+      <h1 className="title">Series Search</h1>
+      <SearchSeries />
     </div>
   );
 }
