553edbd3e6532b9ab86d72028f40392a83486bdc
KaanOzen-mF <kaanozn98@gmail.com>
2023-11-15 10:43:41 +0300

    App.jsx title name changed

--- react-movie-search/src/app.jsx
+++ react-movie-search/src/app.jsx
@@ -4,7 +4,7 @@ import SearchMovies from "./components/SearchMovies";
 export default function App() {
   return (
     <div className="container">
-      <h1 className="title">Test</h1>
+      <h1 className="title">Movie Search</h1>
       <SearchMovies />
     </div>
   );
