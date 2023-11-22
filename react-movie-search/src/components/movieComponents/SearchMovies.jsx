12332b37aa901ddca1a8e19eb51502124841dad2
KaanOzen-mF <kaanozn98@gmail.com>
2023-11-22 14:08:59 +0300

    Loading Animation added

--- react-movie-search/src/components/movieComponents/SearchMovies.jsx
+++ react-movie-search/src/components/movieComponents/SearchMovies.jsx
@@ -7,10 +7,11 @@ import { FaStepBackward } from "react-icons/fa";
 export default function SearchMovies() {
   const [query, setQuery] = React.useState("");
   const [movie, setMovie] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
   const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submitting");
+    setLoading(true);
     const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
@@ -19,6 +20,8 @@ export default function SearchMovies() {
       setMovie(data.results);
     } catch (err) {
       console.error(err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -46,13 +49,20 @@ export default function SearchMovies() {
         </button>
       </form>
 
-      <div className="card-list">
-        {movie
-          .filter((movie) => movie.poster_path)
-          .map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
-          ))}
-      </div>
+      {loading ? (
+        <div className="loader">
+          <div className="loader-filmstrip"></div>
+          <p className="loader-text">Loading</p>
+        </div>
+      ) : (
+        <div className="card-list">
+          {movie
+            .filter((movie) => movie.poster_path)
+            .map((movie) => (
+              <MovieCard movie={movie} key={movie.id} />
+            ))}
+        </div>
+      )}
     </div>
   );
 }
--- react-movie-search/src/components/serieComponents/SearchSeries.jsx
+++ react-movie-search/src/components/serieComponents/SearchSeries.jsx
@@ -7,10 +7,11 @@ import { Link } from "react-router-dom";
 export default function SearchSeries() {
   const [query, setQuery] = React.useState("");
   const [serie, setSerie] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
   const searchSeries = async (e) => {
     e.preventDefault();
-    console.log("submitting");
+    setLoading(true);
     try {
       const url = `https://api.themoviedb.org/3/search/tv?query=${query}&api_key=5dcf7f28a88be0edc01bbbde06f024ab&include_adult=false&language=en-US&page=1`;
 
@@ -21,6 +22,8 @@ export default function SearchSeries() {
       setSerie(data.results);
     } catch (err) {
       console.error(err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -48,13 +51,20 @@ export default function SearchSeries() {
         </button>
       </form>
 
-      <div className="card-list">
-        {serie
-          .filter((serie) => serie.poster_path)
-          .map((serie) => (
-            <SerieCard serie={serie} key={serie.id} />
-          ))}
-      </div>
+      {loading ? (
+        <div className="loader">
+          <div className="loader-filmstrip"></div>
+          <p className="loader-text">Loading</p>
+        </div>
+      ) : (
+        <div className="card-list">
+          {serie
+            .filter((serie) => serie.poster_path)
+            .map((serie) => (
+              <SerieCard serie={serie} key={serie.id} />
+            ))}
+        </div>
+      )}
     </div>
   );
 }
