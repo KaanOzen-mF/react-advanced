9c67b32a7dffe77121bcb4a4dfc8d9b5f484856b
KaanOzen-mF <kaanozn98@gmail.com>
2023-11-23 13:56:24 +0300

    Popup notificaiton compontent added

---
+++ react-movie-search/src/components/PopupNotification.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../popupNotification.css";
+
+const PopupNotification = ({ message, setShowPopup }) => {
+  // Automatically hide the popup after a delay (e.g., 3 seconds)
+  React.useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowPopup(false);
+    }, 3000);
+
+    // Clear the timeout when the component unmounts
+    return () => clearTimeout(timeoutId);
+  }, [setShowPopup]);
+
+  return (
+    <div className="popup-container">
+      <div className="popup-notification">
+        <p>{message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PopupNotification;
