b3a4825db4d7209f3077dd1a661aab754af9562c
KaanOzen-mF <kaanozn98@gmail.com>
2023-11-27 11:13:16 +0300

    Route bugs fixed

--- react-movie-search/src/app.jsx
+++ react-movie-search/src/app.jsx
@@ -3,6 +3,7 @@ import SearchMovies from "./components/movieComponents/SearchMovies";
 import SearchSeries from "./components/serieComponents/SearchSeries";
 import MovieDetail from "./components/movieComponents/MovieDetail";
 
+import Watchlist from "./components/Watchlist";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Layout from "./Layout";
 
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/movies" element={<SearchMovies />} />
         <Route path="/series" element={<SearchSeries />} />
         <Route path="/movies/:id" element={<MovieDetail />} />
+        <Route path="/watchlist" element={<Watchlist />} />
       </Routes>
     </Router>
   );
